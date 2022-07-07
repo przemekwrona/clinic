61c813a98248e67b96628e7576f1ddcc6dd540c4
przemekwrona <przemekwrona25@gmail.com>
2022-07-07 12:24:47 +0200

    feat: init backend layer

---
+++ clinic-service/src/main/java/pl/wrona/clinic/service/ClinicServiceApplication.java
@@ -0,0 +1,13 @@
+package pl.wrona.clinic.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClinicServiceApplication.class, args);
+    }
+
+}
---
+++ clinic-service/src/test/java/pl/wrona/clinic/service/ClinicServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.wrona.clinic.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
