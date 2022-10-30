ae77feb21910708dd1763f5bfef0d8e002f2037b
przemekwrona <przemekwrona25@gmail.com>
2022-10-30 19:33:29 +0100

    feat: refresh after delete

--- clinic-web/src/app/clinic/patient/patient-reports/patient-reports.component.ts
+++ clinic-web/src/app/clinic/patient/patient-reports/patient-reports.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute} from "@angular/router";
 import {Patient} from "../../../services/Profile";
 import {ReportService} from "../../../services/ReportService";
+import {PatientService} from "../../../services/PatientService";
 
 @Component({
   selector: 'app-patient-reports',
@@ -13,7 +14,7 @@ export class PatientReportsComponent implements OnInit {
   public patientCode: string = '';
   public patient: Patient = new Patient();
 
-  constructor(private route: ActivatedRoute, private reportService: ReportService) {
+  constructor(private route: ActivatedRoute, private reportService: ReportService, private patientService: PatientService) {
     this.patientCode = route.snapshot.paramMap.get('patientId') || '';
     this.route.data.subscribe((data) => this.patient = data['patient']);
   }
@@ -23,6 +24,10 @@ export class PatientReportsComponent implements OnInit {
 
   deleteReport(reportId: string) {
     this.reportService.deleteReport(reportId).subscribe(() => {
+      this.patientService.getReports(this.patientCode).subscribe(patient => {
+        // @ts-ignore
+        this.patient = patient;
+      })
     })
   }
 
