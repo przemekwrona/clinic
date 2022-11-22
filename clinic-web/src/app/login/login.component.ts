62ebc3b989dec3aeb8f81e720ab54143bbb5d046
Przemyslaw Wrona <wrona.przemyslaw@eu.equinix.com>
2022-11-22 20:08:31 +0100

    feat: registration to system

--- clinic-web/src/app/login/login.component.ts
+++ clinic-web/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from "@angular/router";
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  public isRegistration = false;
+
+  constructor(private route: ActivatedRoute) {
+  }
 
   ngOnInit(): void {
+    this.route.queryParams.subscribe(params => {
+        this.isRegistration = params['isRegistration'];
+      }
+    );
   }
 
   getYear(): number {
