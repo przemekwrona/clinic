ecae3ba928c77da339259436a0f351ad958148d5
Przemyslaw Wrona <wrona.przemyslaw@eu.equinix.com>
2022-11-22 18:58:03 +0100

    ref: rename UserRegistrationFactory to UserRegistrationMapper

--- clinic-service/src/main/java/pl/wrona/clinic/service/registration/RegistrationService.java
+++ clinic-service/src/main/java/pl/wrona/clinic/service/registration/RegistrationService.java
@@ -2,16 +2,12 @@ package pl.wrona.clinic.service.registration;
 
 import lombok.AllArgsConstructor;
 import org.openapitools.model.UserRegistrationRequest;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import pl.wrona.clinic.service.entity.AppRole;
 import pl.wrona.clinic.service.entity.AppUser;
 import pl.wrona.clinic.service.entity.UserRepository;
 import pl.wrona.clinic.service.role.RoleService;
 
 import javax.transaction.Transactional;
-import java.util.List;
 
 @Service
 @AllArgsConstructor
@@ -20,7 +16,7 @@ public class RegistrationService {
     private final UserRepository userRepository;
     private final RoleService roleService;
 
-    private final UserRegistrationFactory userRegistrationFactory;
+    private final UserRegistrationMapper userRegistrationFactory;
 
     @Transactional
     public void register(UserRegistrationRequest userRegistrationRequest) {
